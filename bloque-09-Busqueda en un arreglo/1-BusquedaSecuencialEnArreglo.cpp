72c8615e755b306eb2136ebce3c073129e92415c
Alt08 <55517300+Alt08@users.noreply.github.com>
2022-03-14 23:16:19 -0600

    Cpp
    FUncionando

--- bloque-09-Busqueda en un arreglo/1-BusquedaSecuencialEnArreglo.cpp
+++ bloque-09-Busqueda en un arreglo/1-BusquedaSecuencialEnArreglo.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
+//Busqueda secuencial
+//a[3,4,2,1,5]; dato = 4;
 
 int main(){
+	int a[] = {3,4,2,1,5};
 
+	char dato = 'a';
+	//Busqueda secuencial
+	int i = 0;
+	char bandera;
+	while((bandera == 'F') && (i<5)){
+		if(a[i] == dato){
+			bandera = 'V';
+		}
+		i++;
+	}
+
+	if(bandera == 'F'){
+		cout << "El numero no existe"<<endl;
+	}else if(bandera == 'V'){
+		cout << "Valor encontrado en la posicion: "<<i-1<<endl;
+	}
 
 	return 0;
 }
--- bloque-09-Busqueda en un arreglo/2-BusquedaBinaria.cpp
+++ bloque-09-Busqueda en un arreglo/2-BusquedaBinaria.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
+//BUSQUEDA BINARIA DE FORMA ASCENDENDE
 
 int main(){
+	int numeros[] = {1,2,3,4,5};
+	int inferior, superior, mitad, dato;
+	char bandera = 'F';
 
+	//dato a buscar
+	dato = 1;
+
+	//Busqueda binaria
+	inferior = 0;
+	superior = 5;
+
+	while (inferior <= superior){
+		mitad = (inferior +superior)/2;
+		if (numeros[mitad] == dato){
+			bandera = 'V';
+			break;
+		}
+		if (numeros[mitad] > dato){
+			superior = mitad;
+			mitad = (inferior+superior)/2;
+		}
+		if(numeros[mitad] < dato){
+			inferior = mitad;
+			mitad = (inferior+superior)/2;
+		}
+	}
+
+	if(bandera == 'V'){
+		cout << "Numero encontrado en posicion: " << mitad << endl;
+	}else{
+		cout << "No se encontrado";
+	}
 
 	return 0;
 }
